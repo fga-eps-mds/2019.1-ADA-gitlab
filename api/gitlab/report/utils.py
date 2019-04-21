313c36c9a2d9bf16a84697ac9c68f6a2d9511508
ErickGiffoni <giffoni.erick@gmail.com>
2019-04-21 15:55:11 -0300

    #135 - update utils.py - stopped at get_jobs and get_members
    Co-authored-By: Lucas Fellipe <lucasfcm9@gmail.com>

--- api/gitlab/report/utils.py
+++ api/gitlab/report/utils.py
@@ -62,16 +62,75 @@ class Report():
             requested_id = response.json()
             return requested_id
 
+    def get_branches(self, project_id, headers):
+        try:
+            response = requests.get("https://gitlab.com/api/"
+                                    "v4/projects/{project_id}/repository/\
+                                     branches"
+                                    .format(project_id=project_id["id"]),
+                                    headers=headers)
+            branches = response.json()
+            branches_names = []
+            for i, item in enumerate(branches):
+                names = {"name": 0}
+                names["name"] = branches[i]["name"]
+                branches_names.append(names)
+            response.raise_for_status()
+        except HTTPError as http_error:
+            dict_error = {"status_code": http_error.response.status_code}
+            raise HTTPError(json.dumps(dict_error))
+        else:
+            return branches_names
+
+    def get_commits(self, project_id, headers):
+        try:
+            response = requests.get("https://gitlab.com/api/"
+                                    "v4/projects/{project_id}/repository/\
+                                     commits".format(project_id=
+                                     project_id["id"]), headers=headers)
+            commits = response.json()
+            filtered_commits = []
+            keys = {"number_of_commits": 0,
+                       "last_commit": {"title": 0,
+                                       "author_name": 0,
+                                       "author_email": 0,
+                                       "authored_date": 0}}
+            keys["last_commit"]["title"] = commits[0]["title"]
+            keys["last_commit"]["author_name"] = commits[0]["author_name"]
+            keys["last_commit"]["author_email"] = commits[0]["author_email"]
+            keys["last_commit"]["authored_date"] = commits[0]["authored_date"]
+            number_of_commits = 0
+            for i, item in enumerate(commits):
+                number_of_commits = number_of_commits + 1
+            keys["number_of_commits"] = number_of_commits
+            filtered_commits.append(keys)
+            response.raise_for_status()
+        except HTTPError as http_error:
+                dict_error = {"status_code": http_error.response.status_code}
+                raise HTTPError(json.dumps(dict_error))
+            else:
+                return filtered_commits
+
+    def get_jobs(project_id, headers):
+        pass
+
+    def get_members(project_id, headers):
+        pass
+
     def generate_report(self, project_owner, project_name):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.GITLAB_API_TOKEN
+        }
         # pegar o id
         project_id = get_project_id(project_owner, project_name);
         # pegar as outras info
-        branches = get_branches(project_id)
-        commits = get_commits(project_id)
-        jobs = get_jobs(project_id)
-        members = get_members(project_id)
-        projects = get_projects(project_id)
-        # retornar para a Ada 
+        branches = get_branches(project_id, headers)
+        commits = get_commits(project_id, headers)
+        jobs = get_jobs(project_id, headers)
+        members = get_members(project_id, headers)
+        projects = get_projects(project_id, headers)
+        # retornar para a Ada
         return jsonify({
             "branches": branches,
             "commits": commits,
