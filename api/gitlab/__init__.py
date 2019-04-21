b2ebf717ac8c742a2d5672f877dd581ed60dca30
ErickGiffoni <giffoni.erick@gmail.com>
2019-04-21 17:07:23 -0300

    #135 - register report_blueprint
    Co-authored-By: Lucas Fellipe <lucasfcm9@gmail.com>

--- api/gitlab/__init__.py
+++ api/gitlab/__init__.py
@@ -25,6 +25,7 @@ def create_app(script_info=None):
     # register blueprints
     app.register_blueprint(pipeline_blueprint)
     app.register_blueprint(build_blueprint)
+    app.register_blueprint(report_blueprint)
 
     # shell context for flask cli
     @app.shell_context_processor
