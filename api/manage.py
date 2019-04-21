4fff3f865ba2a8336615b0d3fb6dfdb98f329e38
João Vitor <joaovytor0@gmail.com>
2019-04-20 23:51:00 -0300

    #137 Add coverage to project

--- api/manage.py
+++ api/manage.py
@@ -1,11 +1,20 @@
-# api/gitlab/manage.py
+import coverage
+
+COV = coverage.coverage(
+    branch=True,
+    include='gitlab/*',
+    omit=[
+        'gitlab/tests/*',
+        'gitlab/config.py',
+    ]
+)
+COV.start()
 
 
 from flask.cli import FlaskGroup
 
 from gitlab import create_app
 import unittest
-
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -19,6 +28,21 @@ def test():
     tests = unittest.TestLoader().discover("gitlab/tests", pattern="test*.py")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('gitlab/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     return 1
 
