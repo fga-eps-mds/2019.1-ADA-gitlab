d471fab9f7697cea4d94f6e6032b4da00eeb6386
ErickGiffoni <giffoni.erick@gmail.com>
2019-05-02 15:32:52 -0300

    #135 fix report route; add database check
    Co-authored-By: Joao Pedro <isudjoao@gmail.com>

--- api/gitlab/report/views.py
+++ api/gitlab/report/views.py
@@ -5,6 +5,8 @@ from gitlab.report.error_messages import *
 import json
 from requests.exceptions import HTTPError
 import os
+from data.user import User
+from data.project import Project
 
 
 report_blueprint = Blueprint("report", __name__)
@@ -19,12 +21,18 @@ def ping_pong():
         "message": "pong!"
     }), 200
 
-@report_blueprint.route("/report/<project_owner>/"
-                          "<project_name>", methods=["GET"])
-def generate_report(project_owner, project_name):
+@report_blueprint.route("/report/<chat_id>", methods=["GET"])
+def generate_report(chat_id):
     try:
-        report = Report(GITLAB_API_TOKEN)
-        generated_report = report.repo_informations(project_owner, project_name)
+        user = User.objects(chat_id=chat_id).first()
+        project = user.project
+        user_has_project = Project.objects(id=project.id)
+        if user_has_project:
+            report = Report(GITLAB_API_TOKEN)
+            generated_report = report.repo_informations(user, project)
+        else:
+            dict_error = {"status_code": 404}
+            raise HTTPError(json.dumps(dict_error))
 
     except HTTPError as http_error:
         dict_message = json.loads(str(http_error))
@@ -32,8 +40,10 @@ def generate_report(project_owner, project_name):
             return jsonify(UNAUTHORIZED), 401
         else:
             return jsonify(NOT_FOUND), 404
-
+    except AttributeError:
+        return jsonify(NOT_FOUND), 404
+        
     else:
         return jsonify(
             generated_report
-        ), 200      
+        ), 200
