14fb0598d86237df23c08934a84e3118f658afa7
vitorandos <torandoing@gmail.com>
2019-06-03 23:37:21 -0300

    #251 Mock build tests

--- api/gitlab/tests/test_build.py
+++ api/gitlab/tests/test_build.py
@@ -1,13 +1,13 @@
 import json
 import unittest
+from unittest.mock import patch
 from gitlab.tests.base import BaseTestCase
 from gitlab.tests.jsonschemas.build.schemas import\
-    valid_schema, unauthorized_schema,\
+    unauthorized_schema,\
     invalid_project_schema, build_valid_schema,\
     build_invalid_schema
 from jsonschema import validate
 from gitlab.build.build_utils import Build
-import os
 from requests.exceptions import HTTPError
 
 
@@ -15,8 +15,27 @@ class TestBuild(BaseTestCase):
     def setUp(self):
         super().setUp()
         self.build = Build(self.user.chat_id)
+        self.get_request_response = [{'id': 222325240,
+                                      'status': 'success',
+                                      'stage': 'test',
+                                      'name': 'unit test',
+                                      'ref': 'master',
+                                      'commit':
+                                      {'title': 'Merge PR',
+                                       'short_id': '717b7ea7'},
+                                      'pipeline':
+                                      {'id': 63936796,
+                                       'ref': 'master',
+                                       'status': 'failed',
+                                       'web_url':
+                                       'https://gitlab.com/'},
+                                      'web_url':
+                                      'https://gitlab.com/',
+                                      }]
 
-    def test_view_get_project_build(self):
+    @patch('gitlab.build.views.Build.get_request')
+    def test_view_get_project_build(self, mocked_build_get_request):
+        mocked_build_get_request.return_value = self.get_request_response
         response = self.client.get("/build/{chat_id}"
                                    .format(chat_id=self.user.chat_id))
         data = json.loads(response.data.decode())
@@ -36,29 +55,35 @@ class TestBuild(BaseTestCase):
         self.assertTrue(unauthorized_json["status_code"], 404)
         validate(invalid_project_json, build_invalid_schema)
 
-    def test_view_get_project_build_invalid_token(self):
-        self.user.access_token = "wrong_token"
-        self.user.save()
+    @patch('gitlab.build.views.Build.get_request')
+    def test_view_get_project_build_http_error(self, mocked_build_get_request):
+        mocked_build_get_request.side_effect = HTTPError
+        chat_id = "8212"
+        self.client.get("/build/{chat_id}".format(chat_id=chat_id))
+        with self.assertRaises(HTTPError):
+            self.build.get_project_build(chat_id)
+
+    @patch('gitlab.utils.gitlab_utils.GitlabUtils.get_access_token')
+    def test_view_get_project_build_invalid_token(self, mocked_access_token):
+        mocked_access_token.return_value = "wrong_token"
         response = self.client.get("/build/{chat_id}"
                                    .format(chat_id=self.user.chat_id))
-        self.user.access_token = os.getenv("GITLAB_API_TOKEN", "")
-        self.user.save()
         invalid_project_json = json.loads(response.data.decode())
         self.assertTrue(response.status_code, 401)
         validate(invalid_project_json, unauthorized_schema)
 
-    def test_get_project_build(self):
+    @patch('gitlab.build.views.Build.get_request')
+    def test_get_project_build(self, mocked_build_get_request):
+        mocked_build_get_request.return_value = self.get_request_response
         requested_build = self.build.get_project_build(self.project.project_id)
-        validate(requested_build, valid_schema)
+        validate(requested_build, build_valid_schema)
 
-    def test_get_project_build_invalid_token(self):
-        self.user.access_token = "wrong_token"
-        self.user.save()
+    @patch('gitlab.utils.gitlab_utils.GitlabUtils.get_access_token')
+    def test_get_project_build_invalid_token(self, mocked_access_token):
+        mocked_access_token.return_value = "wrong_token"
         with self.assertRaises(HTTPError) as context:
             self.build.get_project_build(self.project.project_id)
         unauthorized_json = json.loads(str(context.exception))
-        self.user.access_token = os.getenv("GITLAB_API_TOKEN", "")
-        self.user.save()
         self.assertTrue(unauthorized_json["status_code"], 401)
         validate(unauthorized_json, unauthorized_schema)
 
