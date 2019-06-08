ed11baa3942e73cf89e44ddfab94b556598c54a6
Bruno Dantas <oliveiradantas96@gmail.com>
2019-06-08 12:11:21 -0300

    Create script to verify user domain.

--- api/gitlab/cronjob/scripts/monitoring_domain.py
+++ api/gitlab/cronjob/scripts/monitoring_domain.py
@@ -1,25 +1,31 @@
-from gitlab.utils.gitlab_utils import GitlabUtils
+import requests
+from requests.exceptions import HTTPError
+import json
+import os
+import telegram
 from gitlab.data.user import User
+from gitlab.utils.gitlab_utils import GitlabUtils
 
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "")
 
 class MonitorDomain(GitlabUtils):
     def __init__(self, chat_id):
         super().__init__(chat_id)
 
-    def get_all(self):
-        list(db.User.find({})
-
-    def get_users_domain(self, chat_id):
-        user_domain = self.GITLAB_API_URL +\
-              "user/{chat_id}/"\
-              "domain"\
-              .format(chat_id=chat_id)
-        user_domain_json = self.get_request(user_domain)
-        user_domain_dict = {"user_domain": []}
-        for i, item in enumerate(user_domain_json):
-                _data = {"name": 0}
-            branches_data["name"] = branches_json[i]["name"]
-            branches_dict["branches"].append(branches_data)
-        return user_domain_dict
-
-
+    def verify_domain(self):
+        bot = telegram.Bot(token=ACCESS_TOKEN)
+        user_list = User.objects(domain__ne = None)
+        for user in user_list:
+            status = requests.get(user.domain)
+            if status == 200:
+                pass
+            else:
+                status == 404:
+                bot.send_message(chat_id=chat_id,
+                                text="Seu domínio está fora do ar, "
+                                "verifique o que pode ter ocorrido "
+                                "para que os usuários possam usar "
+                                "sua aplicação normalmente."
+                                reply_markup=reply_markup)
+    return
