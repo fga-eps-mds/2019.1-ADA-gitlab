827f0ab4e3b6784e7f82c730c650a9774ee845dd
João Vitor <joaovytor0@gmail.com>
2019-06-14 12:30:22 -0300

    Update tests Co-authored-By: João Pedro <isudjoao@gmail.com>

--- api/gitlab/stable_deploy/views.py
+++ api/gitlab/stable_deploy/views.py
@@ -1,23 +1,13 @@
 from flask import jsonify, Blueprint
 from flask_cors import CORS
-from gitlab.stable_deploy.utils import StableDeploy
-import json
+from gitlab.stable_deploy.stable_deploy_utils import StableDeploy
 from requests.exceptions import HTTPError
-import os
 from gitlab.data.user import User
-from gitlab.stable_deploy.error_messages import UNAUTHORIZED, NOT_FOUND
+from gitlab.stable_deploy.error_messages import NOT_FOUND
+
 
 stable_deploy_blueprint = Blueprint("stable_deploy", __name__)
 CORS(stable_deploy_blueprint)
-GITLAB_API_TOKEN = os.getenv("GITLAB_API_TOKEN", "")
-
-
-@stable_deploy_blueprint.route("/stable_deploy/ping", methods=["GET"])
-def ping_pong():
-    return jsonify({
-        "status": "success",
-        "message": "pong!"
-    }), 200
 
 
 @stable_deploy_blueprint.route("/stable_deploy/<chat_id>",
@@ -26,17 +16,13 @@ def stable_deploy(chat_id):
     try:
         user = User.objects(chat_id=chat_id).first()
         project = user.project
-        stable_deploy = StableDeploy(GITLAB_API_TOKEN)
+        stable_deploy = StableDeploy(chat_id)
         pipeline_id = stable_deploy.find_latest_stable_version(project.
                                                                project_id)
         deploy_status = stable_deploy.run_stable_deploy(project.project_id,
                                                         pipeline_id)
     except HTTPError as http_error:
-        dict_message = json.loads(str(http_error))
-        if dict_message["status_code"] == 401:
-            return jsonify(UNAUTHORIZED), 401
-        else:
-            return jsonify(NOT_FOUND), 404
+        return stable_deploy.error_message(http_error)
     except AttributeError:
         return jsonify(NOT_FOUND), 404
     else:
